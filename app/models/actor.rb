ca164f03bc40549af5eace6bdab38a9c9e45aa56
Sherry El Garhy <sherryptk@gmail.com>
2018-01-03 00:34:40 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.zip(self.show).collect do |character, show|
-    "#{character} - #{show}"
+    self.characters.collect do |character|
+    "#{character.name} - #{character.show}"
   end
   end
 end
